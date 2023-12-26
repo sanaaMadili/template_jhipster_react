49cb7a1e8ac5f18133ce51603ffbd60604c5f272
sanaaMadili <madili2018@gmail.com>
2023-12-26 12:43:07 +0100

    first commit

---
+++ src/main/java/com/appbiomedicale/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.appbiomedicale.repository;
+
+import com.appbiomedicale.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/appbiomedicale/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.appbiomedicale.service.dto;
